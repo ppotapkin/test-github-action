6190bcbecc589885fb6348504e6568e707a1374e
ppotapkin_wh <pavel.potapkin@grandparade.co.uk>
2024-08-06 14:41:17 +0200

    clean up tests

--- my-app/src/test/java/com/mycompany/app/TestSample.java
+++ my-app/src/test/java/com/mycompany/app/TestSample.java
@@ -10,32 +10,32 @@ import java.util.Optional;
 @Test
 public class TestSample {
 
-    Logger logger = LoggerFactory.getLogger(TestSample.class);
+    private final Logger logger = LoggerFactory.getLogger(TestSample.class);
 
     private final Calculator calculator = new Calculator();
 
     public void testAdding() {
-        Assert.assertEquals(4, calculator.add(2, 2));
+        Assert.assertEquals(calculator.add(2, 2), 4);
         logger.info("Test passed");
     }
 
     public void testSubtracting() {
-        Assert.assertEquals(3, calculator.subtract(5, 2));
+        Assert.assertEquals(calculator.subtract(5, 2), 3);
         logger.info("Test passed");
     }
 
     public void testMultiplication() {
-        Assert.assertEquals(9, calculator.multiply(3, 3));
+        Assert.assertEquals(calculator.multiply(3, 3), 9);
         logger.info("Test passed");
     }
 
     public void testDividing() {
-        Assert.assertEquals(Optional.of(2), calculator.divide(10, 5));
+        Assert.assertEquals(calculator.divide(10, 5), Optional.of(2));
         logger.info("Test passed");
     }
 
     public void testDividingByZero() {
-        Assert.assertEquals(Optional.empty(), calculator.divide(5, 0));
+        Assert.assertEquals(calculator.divide(5, 0), Optional.empty());
         logger.info("Test passed");
     }
 }
